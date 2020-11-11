fc10bbe40073a867ed3ecb1175050f39891649d5
yangyang <444062059@qq.com>
2020-11-11 23:20:27 +0800

    初始化项目

---
+++ src/views/home/Home.vue
@@ -0,0 +1,143 @@
+<template>
+  <div id ="home">
+    <nav-bar class= "home-nav"><div slot ='center'>购物街</div></nav-bar>
+    <home-swiper :banners = "banners"/>
+    <recommend-view :recommends ="recommends" />
+    <feature-view/> 
+    <tab-control class="tab-control" :titles = "['流行','新款','精选']" />
+
+    <ul>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+
+
+    </ul>
+
+    
+  </div>
+</template>
+
+<script> 
+import homeSwiper from './childComps/homeSwiper'
+import RecommendView from './childComps/RecommendView'
+import FeatureView from './childComps/FeatureView'
+
+import NavBar from 'components/common/navbar/NavBar'
+import TabControl from 'components/content/tabControl/TabControl'
+
+import {getHomeMultidata} from "network/home"
+
+
+
+  export default {
+    name: "Home",
+    components: {
+
+      homeSwiper,
+      RecommendView,
+      FeatureView,
+      NavBar,
+      TabControl
+    },
+    data(){
+      return {
+        banners:[],
+        recommends:[],
+        goods:{
+          'pop': {page: 0,list:[]},
+          'news': {page: 0,list:[]},
+          'sell': {page: 0,list:[]}
+        }
+      }
+    },
+    created() {
+      getHomeMultidata().then(res => {
+        console.log(res);
+        this.banners = res.data.banner.list;
+        this.recommends = res.data.recommend.list;
+      })
+    }
+  }
+</script>
+
+<style scoped>
+    #home {
+      padding-top: 44px;
+    }
+
+    .home-nav {
+      background-color: var(--color-tint);
+      color:white;
+      position: fixed;
+      left: 0px;
+      right: 0px;
+      top: 0px;
+      z-index: 9;
+    }
+
+    .tab-control {
+      position: sticky;
+      top:44px;
+    }
+</style>
